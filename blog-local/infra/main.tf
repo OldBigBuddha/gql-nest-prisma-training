b171230fcd685230fad845fa0c2ba9c921894c36
OldBigBuddha <development@oldbigbuddha.net>
2023-01-14 18:47:50 +0900

    chapter-11: add modules fo cloudbuild and cloudsql

--- blog-local/infra/main.tf
+++ blog-local/infra/main.tf
@@ -5,16 +5,17 @@ terraform {
   }
 }
 
+locals {
+  backend_app_name  = "blog-backend-app"
+  frontend_app_name = "blog-frontend-app"
+}
+
 ## project ##
 provider "google" {
   project = var.gcp_project_id
   region  = var.primary_region
 }
 
-locals {
-  backend_app_name  = "blog-backend-app"
-  frontend_app_name = "blog-frontend-app"
-}
 
 # Cloud Run のデプロイで利用するArtifact Registry のリポジトリ
 module "artifact-registry" {
@@ -23,4 +24,23 @@ module "artifact-registry" {
   artifact_registry_location = var.primary_region
   backend_app_name           = local.backend_app_name
   frontend_app_name          = local.frontend_app_name
+}
+
+# Cloud SQL
+module "cloud-sql" {
+  source        = "./modules/cloud-sql"
+  target_region = var.primary_region
+}
+
+# Cloud Build
+# マイグレーション＋バックエンドデプロイ
+# フロントエンドデプロイ
+module "cloud-build" {
+  source                      = "./modules/cloud-build"
+  gcp_project_id              = var.gcp_project_id
+  region                      = var.primary_region
+  cloudsql_instance_full_name = module.cloud-sql.blog_training_db_connection_name
+  backend_app_name            = local.backend_app_name
+  github_owner                = "OldBigBuddha"
+  github_app_repo_name        = "gql-nest-prisma-training"
 }
